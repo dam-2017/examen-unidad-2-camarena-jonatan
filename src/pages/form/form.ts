76c0b4d417ae6208518b386fea91e9caafbafbff
omar camarena <omar.camarena@sinneg.com>
2017-04-28 10:35:55 -0600

    examen2

--- src/pages/form/form.ts
+++ src/pages/form/form.ts
@@ -16,7 +16,8 @@ import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms'
 })
 export class Form {
   public name:any = "";
-  public lastname:any = "";
+  public lastnameP:any = "";
+  public lastnameM:any = "";
   public dob:any = "";
   public rfc:any = "";
 
@@ -25,20 +26,39 @@ export class Form {
   constructor(public navCtrl: NavController, public navParams: NavParams,public formBuilder: FormBuilder) {
     this.myForm = this.formBuilder.group({
       name:['',Validators.required],
-      lastname:['',Validators.required],
+      lastnameP:['',Validators.required],
+      lastnameM:['',Validators.required],
       dob:['',Validators.required],
     });
     this.name = this.myForm.controls['name'];
-    this.lastname = this.myForm.controls['lastname'];
+    this.lastnameP = this.myForm.controls['lastnameP'];
+    this.lastnameM = this.myForm.controls['lastnameM'];
     this.dob = this.myForm.controls['dob'];
   }
   sendData(){
     let data ={
           name: this.name.value,
-          lastname: this.lastname.value,
+          lastnameP: this.lastnameP.value,
+          lastnameM: this.lastnameM.value,
           dob: this.dob.value,
     };
-    console.log(data);
+
+    console.log(data.lastnameP.substring(0,1));
+    console.log(data.lastnameP.split());
+    let vocal;
+    let cad = data.lastnameP.split();
+    for(let i = 0;  i<cad.length; i++ ){
+        console.log(i);
+      if(cad[i] == 'a' || cad[i] == 'e' || cad[i] == 'i' || cad[i] == 'o' || cad[i] == 'u' ){
+        vocal = cad[i];
+        console.log(vocal);
+      }
+
+    }
+    console.log(data.lastnameM.substring(0,1));
+    console.log(data.name.substring(0,1));
+    console.log(data.dob.year);
+
   }
 
   ionViewDidLoad() {
